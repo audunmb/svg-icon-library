981466d09a84ce9abfb95961fab20f0b3b79bc35
Audun Myhra Bergwitz <32357781+audunmb@users.noreply.github.com>
2023-02-02 13:37:16 +0100

    Update svg-icon-library.php

--- svg-icon-library.php
+++ svg-icon-library.php
@@ -2,18 +2,18 @@
 /*
 Plugin Name: SVG Icon library
 Plugin URI: 
-Description: adds custom SVG icons to the Icon block
+Description: Adds custom SVG icons to the Icon block. Requires the Icon Block plugin. 
 Author: audun
 Version: 0.1
 Author URI: 
 License: GPL3
 */
 
-function example_register_custom_icons() {
-	wp_enqueue_script(		'example-register-custom-icons',
+function register_my_custom_icons() {
+	wp_enqueue_script(		'register-my-custom-icons',
 		plugins_url( 'register-custom-icons.js', __FILE__ ),
 		array( 'wp-i18n', 'wp-hooks', 'wp-dom' ), '1.0',
 		true // Very important, otherwise the filter is called too early.
 	);
 }
-add_action( 'enqueue_block_editor_assets', 'example_register_custom_icons' );
+add_action( 'enqueue_block_editor_assets', 'register_my_custom_icons' );
